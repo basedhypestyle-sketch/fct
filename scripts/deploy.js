21c65de49ec1ae3ee55758ada8cd9abf034ec01a
basedhypestyle-sketch <basedhypestyle@gmail.com>
2025-11-17 23:25:27 +0700

    Delete scripts directory

--- scripts/deploy.js
+++
@@ -1,9 +0,0 @@
-async function main(){
-  const [deployer] = await ethers.getSigners();
-  console.log('Deploying with', deployer.address);
-  const F = await ethers.getContractFactory('FIDGhost');
-  const f = await F.deploy();
-  await f.deployed();
-  console.log('FIDGhost deployed to:', f.address);
-}
-main().catch(e=>{console.error(e); process.exit(1);});
